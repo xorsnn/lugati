fd3622053e547d8f2ba1dae81448e9b71f005fa6
xors <xors.nn@gmail.com>
2015-04-10 08:56:24 +0300

    callbacks fix

--- setup.py
+++ setup.py
@@ -1,31 +1,18 @@
 #!/usr/bin/env python
 #coding: utf-8
 __author__ = 'xors'
-
-from distutils.core import setup
+from setuptools import setup, find_packages
+# from distutils.core import setup
 import sys
 
 reload(sys).setdefaultencoding("UTF-8")
 
 setup(
     name='lugati',
-    version='0.1.3',
+    version='0.1.4',
     author='Sergey Grigorev',
     author_email='dev@lugati.ru',
-    packages=[
-        'lugati', 'lugati.lugati_admin',
-        'lugati.lugati_feedback',
-        'lugati.lugati_media', 'lugati.lugati_media.lugati_gallery',
-        'lugati.lugati_mobile', 'lugati.lugati_payment',
-        'lugati.lugati_payment.lugati_coinbase', 'lugati.lugati_payment.lugati_mycelium',
-        'lugati.lugati_payment.lugati_robokassa', 'lugati.lugati_payment.lugati_stripe',
-        'lugati.lugati_points_of_sale', 'lugati.lugati_registration',
-        'lugati.lugati_shop', 'lugati.lugati_shop.lugati_cart',
-        'lugati.lugati_shop.lugati_delivery', 'lugati.lugati_shop.lugati_orders',
-        'lugati.lugati_shop.lugati_pricelist', 'lugati.lugati_shop.lugati_promo',
-        'lugati.lugati_shop.lugati_reports', 'lugati.lugati_static',
-        'lugati.lugati_widgets', 'lugati.products',
-    ],
+    packages=find_packages(),
     url='http://lugati.ru',
     download_url = 'https://github.com/xorsnn/lugati/zipball/master',
     license = 'MIT license',
